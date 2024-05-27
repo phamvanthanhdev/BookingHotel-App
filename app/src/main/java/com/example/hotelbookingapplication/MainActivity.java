e897502e7c4e02a4c7502a22c3ca3f82612a4ffc
Pham Van Thanh <thanhdever@gmail.com>
2024-05-27 23:27:08 +0700

    123 abc

--- app/src/main/java/com/example/hotelbookingapplication/MainActivity.java
+++ app/src/main/java/com/example/hotelbookingapplication/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //abcd
 
         initViews();
         /*listenerHandler();
